29a324b1394aa24d5c2eb7e03301c1a109366065
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-09-25 12:33:28 +0200

    fix: implement FixLegacyDateMixin
    See also https://github.com/acdh-oeaw/apis-core-rdf/issues/295

--- apis_ontology/models.py
+++ apis_ontology/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from apis_core.apis_entities.models import AbstractEntity
 from apis_core.core.models import LegacyDateMixin
+from apis_core.utils import DateParser
 
 
 class LegacyStuffMixin(models.Model):
@@ -14,19 +15,60 @@ class LegacyStuffMixin(models.Model):
         abstract = True
 
 
+# we are overriding the LegacyDateMixin save method for now
+# because the DateParser returns datetime instead of date
+# and the API does not like that
+class FixLegacyDateMixin:
+    def save(self, *args, **kwargs):
+        start = None
+        start_start = None
+        start_end = None
+        end = None
+        end_start = None
+        end_end = None
+
+        if self.start_date_written:
+            start, start_start, start_end = DateParser.parse_date(
+                self.start_date_written
+            )
+            if start:
+                start = start.date()
+            if start_start:
+                start_start = start_start.date()
+            if start_end:
+                start_end = start_end.date()
+
+        if self.end_date_written:
+            end, end_start, end_end = DateParser.parse_date(self.end_date_written)
+            if end:
+                end = end.date()
+            if end_start:
+                end_start = end_start.date()
+            if end_end:
+                end_end = end_end.date()
+
+        self.start_date = start
+        self.start_start_date = start_start
+        self.start_end_date = start_end
+        self.end_date = end
+        self.end_start_date = end_start
+        self.end_end_date = end_end
+
+        super().save(*args, **kwargs)
+
 class Title(models.Model):
     name = models.CharField(max_length=255, blank=True)
 
 
-class Event(LegacyStuffMixin, LegacyDateMixin, AbstractEntity):
+class Event(LegacyStuffMixin, LegacyDateMixin, FixLegacyDateMixin, AbstractEntity):
     kind = models.CharField(max_length=255, blank=True)
 
 
-class Institution(LegacyStuffMixin, LegacyDateMixin, AbstractEntity):
+class Institution(LegacyStuffMixin, LegacyDateMixin, FixLegacyDateMixin, AbstractEntity):
     kind = models.CharField(max_length=255, blank=True)
 
 
-class Person(LegacyStuffMixin, LegacyDateMixin, AbstractEntity):
+class Person(LegacyStuffMixin, LegacyDateMixin, FixLegacyDateMixin, AbstractEntity):
     GENDER_CHOICES = (
         ("female", "female"),
         ("male", "male"),
@@ -38,11 +80,11 @@ class Person(LegacyStuffMixin, LegacyDateMixin, AbstractEntity):
     gender = models.CharField(max_length=15, choices=GENDER_CHOICES, blank=True, null=True)
 
 
-class Place(LegacyStuffMixin, LegacyDateMixin, AbstractEntity):
+class Place(LegacyStuffMixin, LegacyDateMixin, FixLegacyDateMixin, AbstractEntity):
     kind = models.CharField(max_length=255, blank=True)
     lat = models.FloatField(blank=True, null=True, verbose_name="latitude")
     lng = models.FloatField(blank=True, null=True, verbose_name="longitude")
 
 
-class Work(LegacyStuffMixin, LegacyDateMixin, AbstractEntity):
+class Work(LegacyStuffMixin, LegacyDateMixin, FixLegacyDateMixin, AbstractEntity):
     kind = models.CharField(max_length=255, blank=True)
