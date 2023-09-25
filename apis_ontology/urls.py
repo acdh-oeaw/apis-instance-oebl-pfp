9066496dfad5d3819afc77c67526cc57ce51943d
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-09-25 10:40:53 +0200

    fix: use `auth.urls` under `accounts` path
    also move admin site urls above

--- apis_ontology/urls.py
+++ apis_ontology/urls.py
@@ -7,12 +7,13 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from apis_core.apis_entities.api_views import GetEntityGeneric
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("apis/", include("apis_core.urls", namespace="apis")),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path(
         "entity/<int:pk>/", GetEntityGeneric.as_view(), name="GetEntityGenericRoot"
     ),
-    path("admin/", admin.site.urls),
     path("", TemplateView.as_view(template_name="base.html")),
 ]
 
