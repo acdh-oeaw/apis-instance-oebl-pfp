2a51180071f9713a38c1fd98d6c7cd39d5d06a57
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-10-03 16:45:47 +0200

    feat: add Profession to admin interface

--- apis_ontology/admin.py
+++ apis_ontology/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from reversion.admin import VersionAdmin
 
-from .models import Title, Event, Institution, Person, Place, Work, Source, Text
+from .models import Title, Event, Institution, Person, Place, Work, Source, Text, Profession
 
 
-@admin.register(Title, Event, Institution, Person, Place, Work, Source, Text)
+@admin.register(Title, Event, Institution, Person, Place, Work, Source, Text, Profession)
 class OeblAdmin(VersionAdmin):
     pass
