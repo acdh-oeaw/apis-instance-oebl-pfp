1d5037199df278048cc01724dc0a2c7fa6472524
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-10-05 10:16:54 +0200

    fix: add apis_core.core to INSTALLED_APPS
    This is needed with apis-core-rdf >= 0.5.0 because it uses templatetags
    from apis_core.core.

--- apis_ontology/settings.py
+++ apis_ontology/settings.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apis_core.core',
     'apis_core.apis_metainfo',
     'apis_core.apis_vocabularies',
     'apis_core.apis_relations',
