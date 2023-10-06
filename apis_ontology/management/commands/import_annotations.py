62b983077ee61e8f5a0ed64366b1f72ef4fce62b
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-10-06 12:21:26 +0200

    fix: import_annotation script was missing person
    and add more debug statements

--- apis_ontology/management/commands/import_annotations.py
+++ apis_ontology/management/commands/import_annotations.py
@@ -3,7 +3,7 @@ import argparse
 from django.core.management.base import BaseCommand
 
 from apis_highlighter.models import Annotation, AnnotationProject
-from apis_ontology.models import Text
+from apis_ontology.models import Text, Person
 from apis_core.apis_relations.models import TempTriple
 
 #content_type_id = {
@@ -34,13 +34,16 @@ class Command(BaseCommand):
                     try:
                         obj = Person.objects.get(pk=row["object_id"])
                     except Person.DoesNotExist:
+                        print(f"Did not find person with id {row['object_id']}")
                         obj = None
                 else:
                     try:
                         obj = TempTriple.objects.get(pk=row["object_id"])
                     except TempTriple.DoesNotExist:
+                        print(f"Did not find temptriple with id {row['object_id']}")
                         obj = None
                 if obj:
+                    print(obj)
                     ann.content_object = obj
 
                 project, _ = AnnotationProject.objects.get_or_create(pk=row["annotation_project_id"])
