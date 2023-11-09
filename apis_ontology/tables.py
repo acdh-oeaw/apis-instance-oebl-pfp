9a91f5258db2af2c501e59e8995f20f002314d07
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-11-09 13:31:48 +0100

    feat: implement custom table class for person

---
+++ apis_ontology/tables.py
@@ -0,0 +1,26 @@
+import django_tables2 as tables
+from django_tables2.utils import A
+from .models import Person
+
+
+class PersonTable(tables.Table):
+    class Meta:
+        model = Person
+        fields = ["name", "first_name", "start_date", "end_date"]
+
+
+    name = tables.LinkColumn("apis:apis_entities:generic_entities_edit_view", args=[A("self_contenttype.name"), A("pk")], empty_values=[],)
+    first_name = tables.LinkColumn("apis:apis_entities:generic_entities_edit_view", args=[A("self_contenttype.name"), A("pk")], empty_values=[],)
+
+    def render_name(self, record):
+        return record.name or "No name"
+
+    def render_start_date(self, record):
+        if record.start_date:
+            return record.start_date.year
+        return "-"
+
+    def render_end_date(self, record):
+        if record.end_date:
+            return record.end_date.year
+        return "-"
