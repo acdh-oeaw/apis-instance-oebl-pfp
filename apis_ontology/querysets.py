aed7e17ca2446bef92f214eb0d7e9ecdd15ac7bb
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-11-20 19:23:12 +0100

    fix: set collation based on database engine

--- apis_ontology/querysets.py
+++ apis_ontology/querysets.py
@@ -1,6 +1,8 @@
 from django.db.models.functions import Collate
+from django.conf import settings
 
 from .models import Person
 
+DB_COLLATION = 'binary' if 'sqlite' in settings.DATABASES['default']['ENGINE'] else 'en-x-icu'
 
-PersonListViewQueryset = Person.objects.all().order_by(Collate("name", "en-x-icu"))
+PersonListViewQueryset = Person.objects.all().order_by(Collate("name", DB_COLLATION))
