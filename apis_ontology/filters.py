37699439c32f209a912126a7d5f1068744a1a9fb
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-11-29 14:29:30 +0100

    fix: replace custom search with solution based on trigram extension
    Closes: #19

--- apis_ontology/filters.py
+++ apis_ontology/filters.py
@@ -1,42 +1,20 @@
-import logging
-import itertools
 import re
 
-from django.db import models
+from django.contrib.postgres.search import TrigramSimilarity
 
 PATTERN = re.compile(r'''((?:[^ "']|"[^"]*"|'[^']*')+)''')
 
-logger = logging.getLogger(__name__)
+
+def remove_quotes(token):
+    return token.strip('"')
 
 
-def valuetosearchtype(value):
-    if value.strip('"') == value:
-        return "__unaccent__icontains", value
-    return "__exact", value.strip('"')
-
-
-def tokenize(value):
+def trigram_search_filter(queryset, name, value):
     tokens = PATTERN.split(value)
     tokens = list(filter(str.strip, tokens))
-    return list(map(valuetosearchtype, tokens))
-
-
-def name_first_name_filter(queryset, name, value):
-    tokens = tokenize(value)
-    searchfields = ["first_name", "name"]
-
-    q = models.Q()
-    if len(tokens) > 1:
-        for combination in itertools.permutations(tokens, len(searchfields)):
-            q_and = models.Q()
-            mapping = [{field + searchtype: value} for field, (searchtype, value) in zip(searchfields, combination)]
-            for query in mapping:
-                q_and &= models.Q(**query)
-            q |= q_and
-    else:
-        for searchfield in searchfields:
-            mapping = [{searchfield + searchtype: value} for searchtype, value in tokens]
-            for query in mapping:
-                q |= models.Q(**query)
-    logger.debug(q)
-    return queryset.filter(q)
+    tokens = set(list(map(remove_quotes, tokens)) + [value])
+    trig_vector_list = []
+    for token in tokens:
+        trig_vector_list += [TrigramSimilarity("name__unaccent", token) + TrigramSimilarity("first_name__unaccent", token)]
+    trig_vector = sum(trig_vector_list)
+    return queryset.annotate(similarity=trig_vector).filter(similarity__gt=0.5).order_by("-similarity")
