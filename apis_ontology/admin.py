95d21f84f6cb9816fb9fb0baba3efca4c673c465
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-12-13 10:25:12 +0100

    feat: add professioncategory and refactor professionimport to split

--- apis_ontology/admin.py
+++ apis_ontology/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from reversion.admin import VersionAdmin
 
-from .models import Title, Event, Institution, Person, Place, Work, Source, Text, Profession
+from .models import Title, Event, Institution, Person, Place, Work, Source, Text, Profession, ProfessionCategory
 
 
-@admin.register(Title, Event, Institution, Person, Place, Work, Source, Text, Profession)
+@admin.register(Title, Event, Institution, Person, Place, Work, Source, Text, Profession, ProfessionCategory)
 class OeblAdmin(VersionAdmin):
     pass
