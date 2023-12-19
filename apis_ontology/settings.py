99f0503f1ab3e517f9547db6603932ba3ee2e972
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-12-19 13:33:38 +0100

    chore(deps): bump apis-core to 0.9.0 and default settings to 0.1.21
    ... remove uncalled-for settings from settings

--- apis_ontology/settings.py
+++ apis_ontology/settings.py
@@ -5,63 +5,9 @@ from apis_acdhch_default_settings.settings import *
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = True
 
-# Application definition
-
-INSTALLED_APPS = [
-    "apis_override_select2js",
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.postgres',
-    'apis_ontology',
-    'apis_core.core',
-    'apis_core.apis_metainfo',
-    'apis_core.apis_vocabularies',
-    'apis_core.apis_relations',
-    'apis_core.apis_entities',
-    'apis_core.apis_labels',
-    "reversion",
-    # ui stuff
-    "crispy_forms",
-    "django_filters",
-    "django_tables2",
-    "dal",
-    "dal_select2",
-    # api
-    "rest_framework",
-    "rest_framework.authtoken",
-    # for swagger ui generation
-    "drf_spectacular",
-    # highlighter
-    "apis_highlighter",
-]
+INSTALLED_APPS += ["apis_highlighter"]
 
 ROOT_URLCONF = 'apis_ontology.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-STATIC_URL = '/static/'
-STATIC_ROOT = '/tmp/staticfiles'
-
-REDMINE_ID = ""
-
 
 PROJECT_DEFAULT_MD = {}
 
@@ -115,9 +61,6 @@ APIS_ENTITIES = {
             "form": work_form
         },
 }
-
-CRISPY_TEMPLATE_PACK = "bootstrap4"
-DJANGO_TABLES2_TEMPLATE = "django_tables2/bootstrap4.html"
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
