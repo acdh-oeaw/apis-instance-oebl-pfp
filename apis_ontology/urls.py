b361531468eaa64a12997e57241de65304259a32
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-01-19 14:55:49 +0100

    chore: cleanup urls.py
    there was some stuff we don't need anymore

--- apis_ontology/urls.py
+++ apis_ontology/urls.py
@@ -1,5 +1,4 @@
 from django.urls import include
-from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -7,10 +6,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from apis_core.apis_entities.api_views import GetEntityGeneric
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")),
     path("apis/", include("apis_core.urls", namespace="apis")),
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path(
         "entity/<int:pk>/", GetEntityGeneric.as_view(), name="GetEntityGenericRoot"
     ),
@@ -19,12 +15,3 @@ urlpatterns = [
 ]
 
 urlpatterns += staticfiles_urlpatterns()
-
-from rest_framework import routers
-from .api_views import UriViewSet
-router = routers.DefaultRouter()
-router.register('writeuri', UriViewSet, basename='writeuri')
-customurlpatterns = [
-    path('custom-api/', include(router.urls)),
-]
-urlpatterns += customurlpatterns
