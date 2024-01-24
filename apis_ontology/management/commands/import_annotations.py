54626a147ac34a312cf05d894326b0d47cb72f08
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-01-24 09:00:30 +0100

    feat: Add user data to annotations

--- apis_ontology/management/commands/import_annotations.py
+++ apis_ontology/management/commands/import_annotations.py
@@ -5,14 +5,17 @@ from django.core.management.base import BaseCommand
 from apis_highlighter.models import Annotation, AnnotationProject
 from apis_ontology.models import Text, Person
 from apis_core.apis_relations.models import TempTriple
+from django.contrib.auth.models import User
 
-#content_type_id = {
-#        39: # personperson
-#        40: # personplace
-#        41: # personinstitution
-#        8: # person
-#}
-
+user_mapping = {
+        12: 'sennierer',
+        14: 'mkaiser',
+        16: 'ABernad',
+        24: 'PRumpolt',
+        25: 'KLejtovicz',
+        26: 'spacy_user',
+        27: 'APiechl',
+}
 
 class Command(BaseCommand):
     help = "Import data from legacy APIS instance"
@@ -26,6 +29,11 @@ class Command(BaseCommand):
             if row["content_type_id"] and row["object_id"]:
                 ann, _ = Annotation.objects.get_or_create(pk=row["id"], start=row["start"], end=row["end"])
                 ann.orig_string = row["orig_string"]
+                try:
+                    user = User.objects.get(username=user_mapping[int(row["user_added_id"])])
+                    ann.user = user
+                except User.DoesNotExist:
+                    pass
 
                 text = Text.objects.get(pk=row["text_id"])
                 ann.text_content_object = text
