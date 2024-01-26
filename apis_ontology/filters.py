96a11435e42df8e34c49a39cd6dfd94e676434f6
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-01-26 12:34:32 +0100

    fix: combine trigram search with greatest

--- apis_ontology/filters.py
+++ apis_ontology/filters.py
@@ -1,6 +1,7 @@
 import re
 
-from django.contrib.postgres.search import TrigramSimilarity
+from django.contrib.postgres.search import TrigramWordSimilarity
+from django.db.models.functions import Greatest
 
 PATTERN = re.compile(r'''((?:[^ "']|"[^"]*"|'[^']*')+)''')
 
@@ -15,6 +16,7 @@ def trigram_search_filter(queryset, name, value):
     tokens = set(list(map(remove_quotes, tokens)) + [value])
     trig_vector_list = []
     for token in tokens:
-        trig_vector_list += [TrigramSimilarity("name__unaccent", token) + TrigramSimilarity("first_name__unaccent", token)]
-    trig_vector = sum(trig_vector_list)
-    return queryset.annotate(similarity=trig_vector).filter(similarity__gt=0.5).order_by("-similarity")
+        trig_vector_list.append(TrigramWordSimilarity(token, "name"))
+        trig_vector_list.append(TrigramWordSimilarity(token, "first_name"))
+    trig_vector = Greatest(*trig_vector_list)
+    return queryset.annotate(similarity=trig_vector).filter(similarity__gt=0.4).order_by("-similarity")
