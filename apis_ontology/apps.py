9d8d258a93671337cdc055fdba9974e158c6d5bf
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-01-26 15:00:17 +0100

    feat: automatically add users to group

--- apis_ontology/apps.py
+++ apis_ontology/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ApisOntologyConfig(AppConfig):
     default_auto_field = "django.db.models.AutoField"
     name = 'apis_ontology'
+
+    def ready(self):
+        from . import signals
---
+++ apis_ontology/signals.py
@@ -0,0 +1,12 @@
+import os
+from django.contrib.auth.signals import user_logged_in
+from django.dispatch import receiver
+from django.contrib.auth.models import Group
+
+
+@receiver(user_logged_in)
+def add_to_group(sender, user, request, **kwargs):
+    user_list = os.environ.get("AUTH_LDAP_USER_LIST", "").split(",")
+    g1, _ = Group.objects.get_or_create(name='redaktion')
+    if user.username in user_list:
+        g1.user_set.add(user)
