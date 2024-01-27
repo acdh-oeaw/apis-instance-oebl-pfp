c53e61910a915f6a1f5f157500afc60f23b424b6
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-01-27 18:25:39 +0100

    feat: import people from wikidata
    Update importer for new rdf method
    Add rdf parser definition to `rdfimports`
    Add PersonExternalAutocomplete to querysets

--- apis_ontology/importers.py
+++ apis_ontology/importers.py
@@ -1,11 +1,11 @@
 from apis_core.generic.importers import GenericImporter
-from apis_core.utils.rdf import get_modelname_and_dict_from_uri
+from apis_core.utils.rdf import get_definition_and_attributes_from_uri
 
 
 class PersonImporter(GenericImporter):
 
     def request(self, uri):
-        model, data = get_modelname_and_dict_from_uri(uri)
+        model, data = get_definition_and_attributes_from_uri(uri)
         if "profession" in data:
             del data["profession"]
         return data
--- apis_ontology/querysets.py
+++ apis_ontology/querysets.py
@@ -1,3 +1,6 @@
+import json
+import os
+import urllib
 from django.db.models.functions import Collate
 from django.conf import settings
 
@@ -6,3 +9,33 @@ from .models import Person
 DB_COLLATION = 'binary' if 'sqlite' in settings.DATABASES['default']['ENGINE'] else 'en-x-icu'
 
 PersonListViewQueryset = Person.objects.all().order_by(Collate("name", DB_COLLATION), Collate("first_name", DB_COLLATION))
+
+
+class PersonExternalAutocomplete:
+
+    def extract(self, res):
+        url = res["document"]["id"]
+        label = res["document"]["label"]
+        if highlight := res.get("highlight"):
+            if isinstance(highlight.get("label", {}), dict):
+                label = highlight.get("label", {}).get("snippet", "")
+        label += f' <a href="{url}">{url}</a>'
+        return {
+            "id": url,
+            "text": label,
+            "selected_text": label,
+        }
+
+    def get_results(self, q):
+        collectionname = "prosnet-wikidata-person-index"
+        typesensetoken = os.getenv("TYPESENSE_TOKEN", None)
+        typesenseserver = os.getenv("TYPESENSE_SERVER", None)
+        if typesensetoken and typesenseserver:
+            url = f"{typesenseserver}/collections/{collectionname}/documents/search?q={q}&query_by=description&query_by=label"
+            req = urllib.request.Request(url)
+            req.add_header("X-TYPESENSE-API-KEY", typesensetoken)
+            with urllib.request.urlopen(req) as f:
+                data = json.loads(f.read())
+                results = list(map(self.extract, data.get("hits", [])))
+                return results
+        return {}
