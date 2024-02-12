11a9fd277f44bbe80ac7512e4e942239f13fc8d4
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-02-12 12:16:03 +0100

    fix: remove unused settings
    we are not using APIS_ENTITIES anymore

--- apis_ontology/settings.py
+++ apis_ontology/settings.py
@@ -1,5 +1,4 @@
 from apis_acdhch_default_settings.settings import *
-from apis_ontology.filters import trigram_search_filter
 
 DEBUG = True
 
@@ -13,14 +12,6 @@ PROJECT_DEFAULT_MD = {}
 
 CSRF_TRUSTED_ORIGINS = ["https://oebl-pnp.acdh-dev.oeaw.ac.at"]
 
-
-APIS_ENTITIES = {
-        "Person": {
-            "list_filters": {
-                "name": {"method": trigram_search_filter, "label": "Name or first name"},
-            },
-        },
-}
 
 APIS_LIST_LINKS_TO_EDIT = True
 
