c2c51765cb3dcffd2abe2128fad2424b90a9c35b
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-02-12 11:10:56 +0100

    feat: add debug logging to stdout

--- apis_ontology/settings.py
+++ apis_ontology/settings.py
@@ -23,3 +23,24 @@ APIS_ENTITIES = {
 }
 
 APIS_LIST_LINKS_TO_EDIT = True
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    'formatters': {
+       'verbose': {
+           'format': '%(asctime)s %(name)-6s %(levelname)-8s %(message)s',
+       },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+}
