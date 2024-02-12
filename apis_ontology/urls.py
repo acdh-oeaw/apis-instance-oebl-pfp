df8623fb566d36caa2650c4f5f1bfb09481d637e
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-02-12 11:45:27 +0100

    feat: remove customization that are not needed anymore

--- apis_ontology/urls.py
+++ apis_ontology/urls.py
@@ -4,12 +4,10 @@ from django.views.generic import TemplateView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from apis_core.apis_entities.api_views import GetEntityGeneric
-from .views import PersonList
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("apis/entities/entity/person/list/", PersonList.as_view(), name="generic_entities_list"),
     path("apis/", include("apis_core.urls", namespace="apis")),
     path("apis/collections/", include("apis_core.collections.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
