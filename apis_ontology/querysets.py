bc2efad8d0357c74af02c8a2694b97953f24f4d2
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-02-14 10:39:22 +0100

    feat: implement place autocomplete and import
    This refactors the typesense autocomplete stuff out and then adds a
    `apis_ontology.querysets.PlaceExternalAutocomplete`, which inherits from
    the typesense class.
    It also adds a PlaceImporter and a RDF config file for places from
    wikidata.

--- apis_ontology/querysets.py
+++ apis_ontology/querysets.py
@@ -11,7 +11,23 @@ DB_COLLATION = 'binary' if 'sqlite' in settings.DATABASES['default']['ENGINE'] e
 PersonListViewQueryset = Person.objects.all().order_by(Collate("name", DB_COLLATION), Collate("first_name", DB_COLLATION))
 
 
-class PersonExternalAutocomplete:
+class TypeSense_ExternalAutocomplete:
+    def get_results(self, q):
+        typesensetoken = os.getenv("TYPESENSE_TOKEN", None)
+        typesenseserver = os.getenv("TYPESENSE_SERVER", None)
+        if typesensetoken and typesenseserver and getattr(self, "collectionname"):
+            url = f"{typesenseserver}/collections/{self.collectionname}/documents/search?q={q}&query_by=description&query_by=label"
+            req = urllib.request.Request(url)
+            req.add_header("X-TYPESENSE-API-KEY", typesensetoken)
+            with urllib.request.urlopen(req) as f:
+                data = json.loads(f.read())
+                results = list(map(self.extract, data.get("hits", [])))
+                return results
+        return {}
+
+
+class PlaceExternalAutocomplete(TypeSense_ExternalAutocomplete):
+    collectionname = "prosnet-wikidata-place-index"
 
     def extract(self, res):
         url = res["document"]["id"]
@@ -26,16 +42,19 @@ class PersonExternalAutocomplete:
             "selected_text": label,
         }
 
-    def get_results(self, q):
-        collectionname = "prosnet-wikidata-person-index"
-        typesensetoken = os.getenv("TYPESENSE_TOKEN", None)
-        typesenseserver = os.getenv("TYPESENSE_SERVER", None)
-        if typesensetoken and typesenseserver:
-            url = f"{typesenseserver}/collections/{collectionname}/documents/search?q={q}&query_by=description&query_by=label"
-            req = urllib.request.Request(url)
-            req.add_header("X-TYPESENSE-API-KEY", typesensetoken)
-            with urllib.request.urlopen(req) as f:
-                data = json.loads(f.read())
-                results = list(map(self.extract, data.get("hits", [])))
-                return results
-        return {}
+
+class PersonExternalAutocomplete(TypeSense_ExternalAutocomplete):
+    collectionname = "prosnet-wikidata-person-index"
+
+    def extract(self, res):
+        url = res["document"]["id"]
+        label = res["document"]["label"]
+        if highlight := res.get("highlight"):
+            if isinstance(highlight.get("label", {}), dict):
+                label = highlight.get("label", {}).get("snippet", "")
+        label += f' <a href="{url}">{url}</a>'
+        return {
+            "id": url,
+            "text": label,
+            "selected_text": label,
+        }
