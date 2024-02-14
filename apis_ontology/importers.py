689cf877924882e21351d3626fccb84ac9781ee8
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-02-14 10:39:22 +0100

    feat: implement place autocomplete and import
    This refactors the typesense autocomplete stuff out and then adds a
    `apis_ontology.querysets.PlaceExternalAutocomplete`, which inherits from
    the typesense class.
    It also adds a PlaceImporter and a RDF config file for places from
    wikidata.

--- apis_ontology/importers.py
+++ apis_ontology/importers.py
@@ -2,6 +2,12 @@ from apis_core.generic.importers import GenericImporter
 from apis_core.utils.rdf import get_definition_and_attributes_from_uri
 
 
+class PlaceImporter(GenericImporter):
+    def request(self, uri):
+        model, data = get_definition_and_attributes_from_uri(uri)
+        return data
+
+
 class PersonImporter(GenericImporter):
 
     def request(self, uri):
