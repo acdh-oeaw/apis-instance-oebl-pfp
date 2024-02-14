693180c7e36ef3ede72af4798f0b57dbcfcde57c
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-02-14 20:09:52 +0100

    feat(querysets): use structural pattern matching for parsing

--- apis_ontology/querysets.py
+++ apis_ontology/querysets.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import os
 import urllib
 from django.db.models.functions import Collate
@@ -6,12 +7,25 @@ from django.conf import settings
 
 from .models import Person
 
-DB_COLLATION = 'binary' if 'sqlite' in settings.DATABASES['default']['ENGINE'] else 'en-x-icu'
+logger = logging.getLogger(__name__)
+
+DB_COLLATION = ("binary" if "sqlite" in settings.DATABASES["default"]["ENGINE"] else "en-x-icu")
 
 PersonListViewQueryset = Person.objects.all().order_by(Collate("name", DB_COLLATION), Collate("first_name", DB_COLLATION))
 
 
 class TypeSense_ExternalAutocomplete:
+    def extract(self, res):
+        match res:
+            case {"document": {"id": id, "label": text}, **rest}:
+                label = rest.get("highlight", {}).get("label", {})
+                text = label.get("snippet", text) if isinstance(label, dict) else text
+                text += f' <a href="{id}">{id}</a>'
+                return {"id": id, "text": text, "selected_text": text}
+            case unknown:
+                logger.error("Could not parse result from typesense collection %s: %s", self.collectionname, unknown,)
+        return False
+
     def get_results(self, q):
         typesensetoken = os.getenv("TYPESENSE_TOKEN", None)
         typesenseserver = os.getenv("TYPESENSE_SERVER", None)
@@ -21,7 +35,7 @@ class TypeSense_ExternalAutocomplete:
             req.add_header("X-TYPESENSE-API-KEY", typesensetoken)
             with urllib.request.urlopen(req) as f:
                 data = json.loads(f.read())
-                results = list(map(self.extract, data.get("hits", [])))
+                results = list(filter(bool, map(self.extract, data.get("hits", []))))
                 return results
         return {}
 
@@ -29,32 +43,6 @@ class TypeSense_ExternalAutocomplete:
 class PlaceExternalAutocomplete(TypeSense_ExternalAutocomplete):
     collectionname = "prosnet-wikidata-place-index"
 
-    def extract(self, res):
-        url = res["document"]["id"]
-        label = res["document"]["label"]
-        if highlight := res.get("highlight"):
-            if isinstance(highlight.get("label", {}), dict):
-                label = highlight.get("label", {}).get("snippet", "")
-        label += f' <a href="{url}">{url}</a>'
-        return {
-            "id": url,
-            "text": label,
-            "selected_text": label,
-        }
-
 
 class PersonExternalAutocomplete(TypeSense_ExternalAutocomplete):
     collectionname = "prosnet-wikidata-person-index"
-
-    def extract(self, res):
-        url = res["document"]["id"]
-        label = res["document"]["label"]
-        if highlight := res.get("highlight"):
-            if isinstance(highlight.get("label", {}), dict):
-                label = highlight.get("label", {}).get("snippet", "")
-        label += f' <a href="{url}">{url}</a>'
-        return {
-            "id": url,
-            "text": label,
-            "selected_text": label,
-        }
