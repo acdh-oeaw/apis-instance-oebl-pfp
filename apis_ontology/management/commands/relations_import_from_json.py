21c05c4fd3545d3192675846d4fb586f6d19d5aa
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-03-13 13:24:37 +0100

    fix: use contenttype for import script
    instead of `self_contenttype` which seems to be buggy

--- apis_ontology/management/commands/relations_import_from_json.py
+++ apis_ontology/management/commands/relations_import_from_json.py
@@ -1,6 +1,7 @@
 import json
 import pathlib
 from django.core.management.base import BaseCommand
+from django.contrib.contenttypes.models import ContentType
 from apis_core.apis_relations.models import Property, TempTriple
 from apis_core.apis_metainfo.models import RootObject
 
@@ -19,11 +20,13 @@ def import_relations():
             subj = None
             if subj := relation["subj"]:
                 subj = RootObject.objects_inheritance.get_subclass(pk=subj)
-                prop.subj_class.add(subj.self_contenttype)
+                ct = ContentType.objects.get_for_model(subj)
+                prop.subj_class.add(ct)
             obj = None
             if obj := relation["obj"]:
                 obj = RootObject.objects_inheritance.get_subclass(pk=obj)
-                prop.obj_class.add(obj.self_contenttype)
+                ct = ContentType.objects.get_for_model(obj)
+                prop.obj_class.add(ct)
             if subj and obj and prop:
                 try:
                     tt, created = TempTriple.objects.get_or_create(id=id, prop=prop, subj=subj, obj=obj)
