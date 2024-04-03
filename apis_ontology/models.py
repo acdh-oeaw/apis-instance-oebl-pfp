94ad78e834fbeabdd43c4a3fdc044172d3f786f5
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-04-03 13:55:39 +0200

    fix: add denomination entity type

--- apis_ontology/models.py
+++ apis_ontology/models.py
@@ -184,3 +184,10 @@ class Text(GenericModel, models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, blank=True, null=True)
     object_id = models.PositiveIntegerField(blank=True, null=True)
     content_object = GenericForeignKey('content_type', 'object_id')
+
+
+class Dedomination(AbstractEntity):
+    name = models.CharField()
+
+    def __str__(self):
+        return self.name
