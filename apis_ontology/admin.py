88ed68f9cabee9f2a0ab0d24759e11904fa8335e
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-04-08 10:42:52 +0200

    fix: drop reversion from admin

--- apis_ontology/admin.py
+++ apis_ontology/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from reversion.admin import VersionAdmin
 
 from .models import Title, Event, Institution, Person, Place, Work, Source, Text, Profession, ProfessionCategory
 
 
 @admin.register(Title, Event, Institution, Person, Place, Work, Source, Text, Profession, ProfessionCategory)
-class OeblAdmin(VersionAdmin):
+class OeblAdmin(admin.ModelAdmin):
     pass
