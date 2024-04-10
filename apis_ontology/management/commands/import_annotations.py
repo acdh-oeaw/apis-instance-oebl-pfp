92161e07115bb3b056b0ec7e45af3a3ef30812b3
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-04-10 14:21:39 +0200

    refactor: update annotation import for texts as attributes

--- apis_ontology/management/commands/import_annotations.py
+++ apis_ontology/management/commands/import_annotations.py
@@ -1,9 +1,10 @@
 import csv
-import argparse
+import json
+import pathlib
 from django.core.management.base import BaseCommand
 
 from apis_highlighter.models import Annotation, AnnotationProject
-from apis_ontology.models import Text, Person
+from apis_ontology.models import Person
 from apis_core.apis_relations.models import TempTriple
 from django.contrib.auth.models import User
 
@@ -17,14 +18,14 @@ user_mapping = {
         27: 'APiechl',
 }
 
+
 class Command(BaseCommand):
     help = "Import data from legacy APIS instance"
 
-    def add_arguments(self, parser):
-        parser.add_argument("--file", type=argparse.FileType('r'))
-
     def handle(self, *args, **options):
-        reader = csv.DictReader(options["file"])
+        text_to_entity_mapping = json.loads(pathlib.Path("text_to_entity_mapping.json").read_text())
+
+        reader = csv.DictReader(pathlib.Path("data/annotations_oebl_export_10-2023.csv").open())
         for row in reader:
             if row["content_type_id"] and row["object_id"]:
                 ann, _ = Annotation.objects.get_or_create(pk=row["id"], start=row["start"], end=row["end"])
@@ -35,25 +36,30 @@ class Command(BaseCommand):
                 except User.DoesNotExist:
                     pass
 
-                text = Text.objects.get(pk=row["text_id"])
-                ann.text_content_object = text
+                text_id = row["text_id"]
+                entity_map = text_to_entity_mapping.get(text_id, None)
+                if entity_map:
+                    person_id = entity_map["entity_id"]
+                    p = Person.objects.get(pk=person_id)
+                    ann.text_content_object = p
+                    ann.text_field_name = entity_map["field_name"]
 
-                if row["content_type_id"] == 8:
-                    try:
-                        obj = Person.objects.get(pk=row["object_id"])
-                    except Person.DoesNotExist:
-                        print(f"Did not find person with id {row['object_id']}")
-                        obj = None
-                else:
-                    try:
-                        obj = TempTriple.objects.get(pk=row["object_id"])
-                    except TempTriple.DoesNotExist:
-                        print(f"Did not find temptriple with id {row['object_id']}")
-                        obj = None
-                if obj:
-                    print(obj)
-                    ann.content_object = obj
+                    if row["content_type_id"] == 8:
+                        try:
+                            obj = Person.objects.get(pk=row["object_id"])
+                        except Person.DoesNotExist:
+                            print(f"Did not find person with id {row['object_id']}")
+                            obj = None
+                    else:
+                        try:
+                            obj = TempTriple.objects.get(pk=row["object_id"])
+                        except TempTriple.DoesNotExist:
+                            print(f"Did not find temptriple with id {row['object_id']}")
+                            obj = None
+                    if obj:
+                        print(obj)
+                        ann.content_object = obj
 
-                project, _ = AnnotationProject.objects.get_or_create(pk=row["annotation_project_id"])
-                ann.project = project
-                ann.save()
+                    project, _ = AnnotationProject.objects.get_or_create(pk=row["annotation_project_id"])
+                    ann.project = project
+                    ann.save()
