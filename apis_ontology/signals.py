24ae053cdd77822c3bb3a204953b7e42d190f2cb
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-04-11 10:19:45 +0200

    refactor: drop text model
    It was replace by fields in the Person model

--- apis_ontology/signals.py
+++ apis_ontology/signals.py
@@ -2,16 +2,7 @@ import os
 from django.contrib.auth.signals import user_logged_in
 from django.dispatch import receiver
 from django.contrib.auth.models import Group
-from apis_core.apis_entities.signals import post_merge_with
 
-from apis_ontology.models import Text
-from apis_highlighter.models import Annotation
-from django.contrib.contenttypes.models import ContentType
-
-
-import logging
-
-logger = logging.getLogger(__name__)
 
 
 @receiver(user_logged_in)
@@ -20,30 +11,3 @@ def add_to_group(sender, user, request, **kwargs):
     g1, _ = Group.objects.get_or_create(name='redaktion')
     if user.username in user_list:
         g1.user_set.add(user)
-
-
-@receiver(post_merge_with)
-def merge_texts(sender, instance, entities, **kwargs):
-    for entity in entities:
-        logger.info(f"Copying texts from {entity} to {instance}")
-        content_type = ContentType.objects.get_for_model(entity)
-        for text in Text.objects.filter(content_type=content_type, object_id=entity.id):
-            text_content_type = ContentType.objects.get_for_model(text)
-            if itext := Text.objects.filter(content_type=content_type, object_id=instance.id, kind=text.kind).first():
-                # The order of steps here is important
-                # We first append to the existing text and only *then*
-                # move the pointer of the old annotations to the existing
-                # text. If we do it the other way around, the annotation
-                # signal would fire and the offsets would be miscalculated
-                textlen = len(itext.text + "\n\n")
-                itext.text += "\n\n" + text.text
-                itext.save()
-                for annotation in Annotation.objects.filter(text_content_type=text_content_type, text_object_id=text.id):
-                    annotation.start = annotation.start + textlen
-                    annotation.end = annotation.end + textlen
-                    annotation.text_object_id = itext.id
-                    annotation.save()
-                text.delete()
-            else:
-                text.object_id = instance.id
-                text.save()
