141f48aa533c53e92ce0c2d23e4600cf65df2213
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-04-16 15:00:26 +0200

    feat(import): implement getting timestamps from revisions

--- apis_ontology/management/commands/relations_import_from_json.py
+++ apis_ontology/management/commands/relations_import_from_json.py
@@ -1,35 +1,20 @@
+import datetime
 import json
 import pathlib
-import datetime
 from django.core.management.base import BaseCommand
 from django.contrib.contenttypes.models import ContentType
 from apis_core.apis_relations.models import Property, TempTriple
 from apis_core.apis_metainfo.models import RootObject
 
 
-def parse_source_date(source):
-    date = None
-    if source.pubinfo.startswith("\u00d6BL 1815-1950, Bd. ") or source.pubinfo.startswith("\u00d6BL Online-Edition, Bd."):
-        close_pos = source.pubinfo.find(")")
-        date = source.pubinfo[close_pos-4:close_pos]
-        date = datetime.datetime(int(date), 1, 1)
-    if source.pubinfo.startswith("\u00d6BL Online-Edition, Lfg."):
-        close_pos = source.pubinfo.find(")")
-        # we should parse the whole date
-        day, month, year = source.pubinfo[close_pos-10:close_pos].split(".")
-        date = datetime.datetime(int(year), int(month), int(day))
-    return date
-
-
-def timestamp(obj: object):
-    date = None
-    if obj.sources.exists():
-        date = parse_source_date(obj.sources.first())
-    return date
+def is_relation(revision_tuple, rel_id, rel_type):
+    r_id, revision = revision_tuple
+    return r_id == rel_id and revision["model"] == rel_type
 
 
 def import_relations():
     relations = json.loads(pathlib.Path("relations.json").read_text())
+    revisions = json.loads(pathlib.Path("data/reversion.json").read_text())
 
     l = len(relations)
     p = 0
@@ -50,18 +35,15 @@ def import_relations():
                 ct = ContentType.objects.get_for_model(obj)
                 prop.obj_class.add(ct)
             if subj and obj and prop:
-                dates = []
-                if (obj_date := timestamp(obj)) is not None:
-                    dates.append(obj_date)
-                if (subj_date := timestamp(subj)) is not None:
-                    dates.append(subj_date)
                 try:
                     tt, created = TempTriple.objects.get_or_create(id=id, prop=prop, subj=subj, obj=obj)
                     for attribute in relation:
                         if hasattr(tt, attribute) and attribute not in ["id", "subj", "obj"]:
                             setattr(tt, attribute, relation[attribute])
-                    if dates:
-                        tt._history_date = min(dates)
+                    rel_revisions = list(filter(lambda x: is_relation(x, str(id), relation["type"]), revisions.items()))
+                    if rel_revisions:
+                        rid, revision = rel_revisions[0]
+                        tt._history_date = datetime.datetime.fromisoformat(revision["timestamp"])
                     tt.save()
                 except Exception as e:
                     print(e)
