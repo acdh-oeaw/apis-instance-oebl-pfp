116aa3f7215b4958d96a78b69a16622f99c54f3e
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-04-17 13:54:06 +0200

    refactor(management): comment print statement

--- apis_ontology/management/commands/relations_import_from_json.py
+++ apis_ontology/management/commands/relations_import_from_json.py
@@ -98,7 +98,7 @@ def import_relations():
 
     for id, relation in relations.items():
         p += 1
-        print(f"{p}/{l}:\t {id}: {relation['name']}")
+        #print(f"{p}/{l}:\t {id}: {relation['name']}")
         prop, created = Property.objects.get_or_create(name_forward=relation["name"], name_reverse=relation["name_reverse"])
         try:
             subj = None
