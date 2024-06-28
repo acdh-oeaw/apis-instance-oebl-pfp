d2d7a7a5598c1c3dca4c7019733495a0913c4fb9
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-06-28 13:36:16 +0200

    feat(querysets): use custom filter for InstitutionAutocomplete

--- apis_ontology/querysets.py
+++ apis_ontology/querysets.py
@@ -2,6 +2,10 @@ import logging
 import os
 from django.db.models.functions import Collate
 from django.conf import settings
+from django.db.models import Value
+from django.contrib.postgres.search import TrigramSimilarity
+from django.db.models import Case, When, FloatField
+from django.db.models.functions import Greatest, Length
 
 from apis_core.utils.autocomplete import ExternalAutocomplete, TypeSenseAutocompleteAdapter, LobidAutocompleteAdapter
 
@@ -14,6 +18,21 @@ DB_COLLATION = ("binary" if "sqlite" in settings.DATABASES["default"]["ENGINE"] 
 
 def PersonListViewQueryset(*args):
     return Person.objects.all().order_by(Collate("surname", DB_COLLATION), Collate("forename", DB_COLLATION))
+
+
+def InstitutionAutocompleteQueryset(model, query):
+    # We use two ranking approaches:
+    # we check if the query is contained in the result, if so we
+    # calculate a rank based on the difference in length
+    # if the query is *not* contained in the result, we
+    # use the trigram similarity score
+    insitutions = model.objects.annotate(
+            icontains_rank=Case(
+                When(name__unaccent__icontains=query, then=Value(10.0) / (Length("name", output_field=FloatField()) - Value(len(query)) + Value(1)))
+            ),
+            similarity=TrigramSimilarity("name", query)
+            ).annotate(rank=Greatest("icontains_rank", "similarity")).order_by("-rank")
+    return insitutions
 
 
 class PlaceExternalAutocomplete(ExternalAutocomplete):
