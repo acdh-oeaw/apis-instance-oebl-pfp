81802255a9fbfbd74f9ddf3705ccd990c0f36af5
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-07-01 11:11:02 +0200

    chore(deps): bump apis-core to v0.23.0
    The `GenericImporter` from apis-core was renamed to
    `GenericModelImporter` and is thus picked automatically for all the
    models inheriting from `GenericModel`. This means we don't have to
    override it, if we don't want to change its functionality. It now also
    automatically tries to import using RDF, therefore we can drop our
    overrides for Place and Institution. For Person we still override the
    importer, to be able to mangle that data before import, but at least the
    inheriting `PersonImporter` now gets a bit smaller.

--- apis_ontology/importers.py
+++ apis_ontology/importers.py
@@ -1,23 +1,8 @@
-from apis_core.generic.importers import GenericImporter
-from apis_core.utils.rdf import get_definition_and_attributes_from_uri
-from apis_ontology.models import Person, Place, Institution
+from apis_core.generic.importers import GenericModelImporter
 
 
-class PlaceImporter(GenericImporter):
-    def request(self, uri):
-        model, data = get_definition_and_attributes_from_uri(uri, Place)
-        return data
-
-
-class PersonImporter(GenericImporter):
-    def request(self, uri):
-        model, data = get_definition_and_attributes_from_uri(uri, Person)
+class PersonImporter(GenericModelImporter):
+    def mangle_data(self, data):
         if "profession" in data:
             del data["profession"]
-        return data
-
-
-class InstitutionImporter(GenericImporter):
-    def request(self, uri):
-        model, data = get_definition_and_attributes_from_uri(uri, Institution)
         return data
