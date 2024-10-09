298503741b0a73e788148b1f871a6910ab0b9e7c
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2024-10-09 15:43:40 +0200

    fix: change to signoz django example

--- gunicorn.config.py
+++ gunicorn.config.py
@@ -1,7 +1,19 @@
-from opentelemetry.instrumentation.django import DjangoInstrumentor
+import os
+
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 
 def post_fork(server, worker):
     server.log.info("Worker spawned (pid: %s)", worker.pid)
 
-    DjangoInstrumentor().instrument()
+    resource = Resource.create(attributes={"service.name": "apis-oebl-pfp"})
+
+    trace.set_tracer_provider(TracerProvider(resource=resource))
+    span_processor = BatchSpanProcessor(
+        OTLPSpanExporter(endpoint=os.environ.get("OTEL_EXPORTER_OTLP_ENDPOINT"))
+    )
+    trace.get_tracer_provider().add_span_processor(span_processor)
