d4fab1e5eb216863d63c86ea5e89b3de95961360
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2024-10-09 16:24:44 +0200

    fix: add db calls

--- gunicorn.config.py
+++ gunicorn.config.py
@@ -5,15 +5,24 @@ from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExport
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.instrumentation.django import DjangoInstrumentor
+from opentelemetry.instrumentation.psycopg2 import Psycopg2Instrumentor
 
 
 def post_fork(server, worker):
     server.log.info("Worker spawned (pid: %s)", worker.pid)
 
     resource = Resource.create(attributes={"service.name": "apis-oebl-pfp"})
-
-    trace.set_tracer_provider(TracerProvider(resource=resource))
+    provider = TracerProvider(resource=resource)
+    trace.set_tracer_provider(provider)
     span_processor = BatchSpanProcessor(
         OTLPSpanExporter(endpoint=os.environ.get("OTEL_EXPORTER_OTLP_ENDPOINT"))
     )
     trace.get_tracer_provider().add_span_processor(span_processor)
+    DjangoInstrumentor().instrument(
+        tracer_provider=provider,
+        is_sql_commentor_enabled=True,
+    )
+    Psycopg2Instrumentor().instrument(
+        tracer_provider=provider, skip_dep_check=True, enable_commenter=True
+    )
