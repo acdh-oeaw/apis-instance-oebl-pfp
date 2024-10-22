919a92ca12d5387d2ea4f97c22ed6338f896ef24
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2024-10-22 09:32:04 +0200

    feat: add basic cidoc serializer

--- apis_ontology/renderer.py
+++ apis_ontology/renderer.py
@@ -1,70 +1,45 @@
 from rest_framework import renderers
-from rdflib import Graph, Literal, URIRef
-from rdflib.namespace import RDF, RDFS, XSD
+from rdflib import Graph
 
 
-class CIDOCCRMRenderer(renderers.BaseRenderer):
-    media_type = "application/rdf+xml"
-    format = "rdf"
+class BaseRdfRenderer(renderers.BaseRenderer):
+    def render(self, data, accepted_media_type=None, renderer_context=None):
+        result = Graph()
+
+        if isinstance(data, dict) and "results" in data:
+            # Handle case where data is a dict with multiple graphs
+            for graph in data["results"]:
+                if isinstance(graph, Graph):
+                    result += graph
+        elif isinstance(data, Graph):
+            # Handle case where data is a single graph
+            result = data
+        else:
+            raise ValueError(
+                "Invalid data format. Expected rdflib Graph or dict with 'results' key containing graphs"
+            )
+        return result
+
+
+class CidocTTLRenderer(BaseRdfRenderer):
+    format = "Cidoc"
+    media_type = "text/ttl"
 
     def render(self, data, accepted_media_type=None, renderer_context=None):
-        g = Graph()
-
-        # Define namespaces
-        crm = g.namespace("http://www.cidoc-crm.org/cidoc-crm/")
-        ex = g.namespace("http://example.org/")
-
-        # Create the Person instance
-        person_uri = URIRef(ex[str(data["id"])])
-        g.add((person_uri, RDF.type, crm.E21_Person))
-
-        # Add properties
-        appellation_uri = URIRef(ex[f"appellation_{data['id']}"])
-        g.add((appellation_uri, RDF.type, crm.E41_Appellation))
-        g.add((person_uri, crm.P1_is_identified_by, appellation_uri))
-        g.add(
-            (
-                appellation_uri,
-                RDFS.label,
-                Literal(f"{data['forename']} {data['surname']}"),
-            )
+        return (
+            super()
+            .render(data, accepted_media_type, renderer_context)
+            .serialize(format="ttl")
         )
 
-        if "forename" in data:
-            forename_uri = URIRef(ex[f"forename_{data['id']}"])
-            g.add((forename_uri, RDF.type, crm.E41_Appellation))
-            g.add((appellation_uri, crm.P106_is_composed_of, forename_uri))
-            g.add((forename_uri, RDFS.label, Literal(data["forename"])))
 
-        if "surname" in data:
-            surname_uri = URIRef(ex[f"surname_{data['id']}"])
-            g.add((surname_uri, RDF.type, crm.E41_Appellation))
-            g.add((appellation_uri, crm.P106_is_composed_of, surname_uri))
-            g.add((surname_uri, RDFS.label, Literal(data["surname"])))
+class CidocXMLRenderer(BaseRdfRenderer):
+    format = "Cidoc"
+    media_type = "application/rdf+xml"
 
-        if "birth_date" in data:
-            birth_event = URIRef(ex[f"birth_{data['id']}"])
-            g.add((birth_event, RDF.type, crm.E67_Birth))
-            g.add((birth_event, crm.P98_brought_into_life, person_uri))
-            g.add(
-                (
-                    birth_event,
-                    crm.P4_has_time_span,
-                    Literal(data["birth_date"], datatype=XSD.date),
-                )
-            )
-
-        if "death_date" in data:
-            death_event = URIRef(ex[f"death_{data['id']}"])
-            g.add((death_event, RDF.type, crm.E69_Death))
-            g.add((death_event, crm.P100_was_death_of, person_uri))
-            g.add(
-                (
-                    death_event,
-                    crm.P4_has_time_span,
-                    Literal(data["death_date"], datatype=XSD.date),
-                )
-            )
-
-        # Serialize the graph to RDF/XML
-        return g.serialize(format="xml")
+    def render(self, data, accepted_media_type=None, renderer_context=None):
+        return (
+            super()
+            .render(data, accepted_media_type, renderer_context)
+            .serialize(format="xml")
+        )
