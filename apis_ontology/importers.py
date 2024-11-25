2cb8a41d4c7b8a2a3c344bf0ad84868f79d5a56e
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2024-11-25 12:42:47 +0100

    fix: add RDF import for events
    resolves #374

--- apis_ontology/importers.py
+++ apis_ontology/importers.py
@@ -1,4 +1,6 @@
 from apis_core.generic.importers import GenericModelImporter
+from apis_core.utils.helpers import create_object_from_uri
+from apis_ontology.models import FandStattIn, Place
 
 
 class PersonImporter(GenericModelImporter):
@@ -6,3 +8,15 @@ class PersonImporter(GenericModelImporter):
         if "profession" in data:
             del data["profession"]
         return data
+
+
+class EventImporter(GenericModelImporter):
+    def create_instance(self):
+        data = self.get_data(drop_unknown_fields=False)
+        modelfields = [field.name for field in self.model._meta.fields]
+        data_croped = {key: data[key] for key in data if key in modelfields}
+        subj = self.model.objects.create(**data_croped)
+        if "related_place" in data:
+            place = create_object_from_uri(data["related_place"], Place)
+            FandStattIn.objects.create(subj=subj, obj=place)
+        return subj
