9abe20ac6e94aaa77450ce907a10d73e19eb54b1
Birger Schacht <birger.schacht@oeaw.ac.at>
2026-07-16 09:03:08 +0200

    feat(apis_ontology): add ListRelationTypesAPIView endpoint

--- apis_ontology/urls.py
+++ apis_ontology/urls.py
@@ -2,9 +2,13 @@ from apis_acdhch_default_settings.urls import urlpatterns
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 
+from apis_ontology.api_views import ListRelationTypesAPIView
+
 urlpatterns += [
     path("highlighter/", include("apis_highlighter.urls", namespace="highlighter")),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += [path("", include("django_interval.urls"))]
+
+urlpatterns += [path("apis/api/listrelationtypes", ListRelationTypesAPIView.as_view())]
