bf3eb49812437764783f2ea84db3fc41af4beb5a
Birger Schacht <birger.schacht@oeaw.ac.at>
2026-09-16 07:56:02 +0200

    feat(tables): override professions table to show count of persons

--- apis_ontology/tables.py
+++ apis_ontology/tables.py
@@ -1,4 +1,5 @@
 import django_tables2 as tables
+from django.db.models import Count
 
 from apis_core.generic.tables import CustomTemplateColumn, GenericTable
 from apis_core.relations.tables import RelationsListTable
@@ -68,3 +69,15 @@ class OEBLBaseEntityRelationsTable(RelationsListTable):
                 "style": "max-width:30vw; word-wrap:break-word; overflow-wrap:break-word; white-space:normal;"
             }
         }
+
+
+class ProfessionTable(GenericTable):
+    count = tables.Column(empty_values=())
+
+    def order_count(self, queryset, is_descending=False):
+        queryset = queryset.annotate(person_count=Count("person"))
+        queryset = queryset.order_by(("-" if is_descending else "") + "person_count")
+        return queryset, True
+
+    def render_count(self, value, record):
+        return record.person_set.count()
